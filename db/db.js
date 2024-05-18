710751e141544c2cd442ab6b313e05d8c273df70
Dominika <dominikachruslinska98@gmail.com>
2024-05-18 17:17:20 +0200

    Created tabled drinks, ingredients, images

--- db/db.js
+++ db/db.js
@@ -9,4 +9,39 @@ db.run(`
     )
 `);
 
+db.run(`
+    CREATE TABLE IF NOT EXISTS drinks ( 
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        recipe TEXT NOT NULL
+    )
+`)
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS ingredients ( 
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE
+    )
+`)
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS drink_ingredients ( 
+        drinkId INTEGER,
+        ingredientId INTEGER,
+        quantity TEXT,
+        FOREIGN KEY (drinkId) REFERENCES drinks(id),
+        FOREIGN KEY (ingredientId) REFERENCES ingredients(id),
+        PRIMARY KEY (drinkId, ingredientId)
+    )
+`)
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS images ( 
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        drinkId INTEGER,
+        path TEXT NOT NULL,
+        FOREIGN KEY (drinkId) REFERENCES drinks(id)
+    )
+`)
+
 module.exports = db;
