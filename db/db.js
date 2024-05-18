00eb29dd352c2c154e815e80a9993837d57038bc
Dominika <dominikachruslinska98@gmail.com>
2024-05-18 19:00:06 +0200

    Added drinks json and insert query

--- db/db.js
+++ db/db.js
@@ -1,47 +1,70 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('.bar_assistant.db');
+const drinks = require('./data/drinks.json');
 
 db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS users
+    (
+        id       INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
+        password TEXT        NOT NULL
     )
 `);
 
 db.run(`
-    CREATE TABLE IF NOT EXISTS drinks ( 
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
+    CREATE TABLE IF NOT EXISTS drinks
+    (
+        id     INTEGER PRIMARY KEY AUTOINCREMENT,
+        name   TEXT NOT NULL,
         recipe TEXT NOT NULL
     )
 `)
 
 db.run(`
-    CREATE TABLE IF NOT EXISTS ingredients ( 
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS ingredients
+    (
+        id   INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL UNIQUE
     )
 `)
 
 db.run(`
-    CREATE TABLE IF NOT EXISTS drink_ingredients ( 
-        drinkId INTEGER,
+    CREATE TABLE IF NOT EXISTS drink_ingredients
+    (
+        drinkId      INTEGER,
         ingredientId INTEGER,
-        quantity TEXT,
-        FOREIGN KEY (drinkId) REFERENCES drinks(id),
-        FOREIGN KEY (ingredientId) REFERENCES ingredients(id),
+        quantity     TEXT,
+        FOREIGN KEY (drinkId) REFERENCES drinks (id),
+        FOREIGN KEY (ingredientId) REFERENCES ingredients (id),
         PRIMARY KEY (drinkId, ingredientId)
     )
 `)
 
 db.run(`
-    CREATE TABLE IF NOT EXISTS images ( 
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS images
+    (
+        id      INTEGER PRIMARY KEY AUTOINCREMENT,
         drinkId INTEGER,
-        path TEXT NOT NULL,
-        FOREIGN KEY (drinkId) REFERENCES drinks(id)
+        path    TEXT NOT NULL,
+        FOREIGN KEY (drinkId) REFERENCES drinks (id)
     )
 `)
+
+// sql for inserting multiple rows
+const sql = 'INSERT INTO drinks (id, name, recipe) VALUES ' +
+    drinks.map(() => '(?, ?, ?)').join(', ');
+
+const values = drinks.reduce((acc, drink) => {
+    acc.push(drink.id, drink.name, drink.recipe);
+    return acc;
+}, []);
+
+// insert array
+db.run(sql, values, function (err) {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log(`Rows inserted: ${this.changes}`);
+});
 
 module.exports = db;
